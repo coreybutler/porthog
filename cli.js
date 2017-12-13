7d422d4b8d35aae479920c598e949682f6fbe6bd
Corey Butler <corey@coreybutler.com>
2017-12-13 00:09:02 -0600

    Better support for multiprocess port configurations.

--- cli.js
+++ cli.js
@@ -33,17 +33,43 @@ if (data.length === 0) {
 }
 
 console.log('\nPORT      PID       PROCESS\n--------------------------------------------------')
-data.forEach(item => {
+const format = (item) => {
   let str = item.port
+
   for (let i = 0; i < (10 - item.port.length); i++) {
     str = str + ' '
   }
+
   str = str + item.pid
+
   for (let i = 0; i < (10 - item.pid.length); i++) {
     str = str + ' '
   }
+
   str = str + item.process.replace(/\n{1,10}/, '') + (item.user.trim().length > 0 ? ' (' + item.user + (item.user.trim().toLowerCase() === 'unknown' ? ' User' : '') + ')' : '')
 
   console.log(str)
+}
+
+data.forEach(item => {
+  if (!item.pid) {
+    try {
+      let port = item.port
+      delete item.port
+
+      Object.keys(item).forEach(pid => {
+        format({
+          port: port,
+          pid: pid,
+          process: item[pid].process,
+          user: item[pid].user
+        })
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  } else {
+    format(item)
+  }
 })
 console.log('\n')
